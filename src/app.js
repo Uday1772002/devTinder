29ad53ee63798b0e3a34c9a8c5ef6e08513f7baf
Jayaram Uday <jayaramuday17@gmail.com>
2025-10-18 20:08:41 +0530

    playingWithRouters

--- src/app.js
+++ src/app.js
@@ -2,23 +2,35 @@ const express = require("express");
 
 const app = express(); //create server
 
-app.use("/", (req, res) => {
+app.use("/", (req, res, next) => {
+  console.log("Middleware executed");
+  // res.send("Hello World");
+  next();
+});
+
+app.get("/user", (req, res, next) => {
+  res.send({ name: "John", age: 30 });
+  next();
+});
+
+app.use("/test", (req, res) => {
   res.send("Welcome to the home page!");
 });
 
-app.use("/yo", (req, res) => {
-  res.send("Hello from Express!");
-});
-
-app.use("/re", function (req, res) {
-  res.send("Sorry, that route doesn't exist.");
-});
-
-app.use("/red", (req, res) => {
-  res.send("So that route doesn't exist.");
-});
+app.use(
+  "/User",
+  (req, res, next) => {
+    console.log("First callback function executed");
+    next();
+    // res.send("Yo man Welcome");
+  },
+  (req, res, next) => {
+    console.log("Second callback function executed");
+    res.send("This is the second callback function");
+    next();
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-3;

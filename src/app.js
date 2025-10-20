beec2f9b2fdf8ae3571f398489d1f82c9be65910
Jayaram Uday <jayaramuday17@gmail.com>
2025-10-20 17:14:50 +0530

    getApis

--- src/app.js
+++ src/app.js
@@ -7,8 +7,28 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
+  console.log(user);
   await user.save();
   res.send("User signed up successfully");
+});
+
+app.get("/user", async (req, res) => {
+  const userMail = req.body.email;
+  const user = await User.find({ email: userMail });
+  if (user.length === 0) {
+    return res.status(404).send("User not found");
+  }
+  res.send(user);
+});
+
+app.delete("/userd", async (req, res) => {
+  const deleteMail = req.body.email;
+  try {
+    const deleteUser = await User.deleteOne({ email: deleteMail });
+    res.send(deleteUser);
+  } catch (err) {
+    res.status(500).send("Error deleting user");
+  }
 });
 
 connectDB()

81c03f4819cdddb14f18bc08414d4220d5c6ab3d
M.Jayaram uday <jayaramuday17@gmail.com>
2026-03-30 00:03:34 +0530

    Refactor database connection string
    Updated MongoDB connection string to remove sensitive information.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://jayaramuday24_db_user:GETeEVFLD2KBTrMV@cluster0.hvdeeyq.mongodb.net/devTinder?appName=Cluster0"
+    "mongodb+srv://jayaramuday24_db_user"
   );
 };
 
